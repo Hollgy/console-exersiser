5ef66da57ff3d60e91a4144080d26d07dce2fb55
KG <karlgustav.ek@hotmail.com>
2023-01-17 09:54:04 +0100

    push av arbeten tom 5:5

--- script.js
+++ script.js
@@ -1,6 +1,102 @@
 import { question } from 'readline-sync'
 
 
+
+// -------------------------------------------------
+// UPPGIFT 5:5
+const number = Number(question("Enter a number: "));
+
+//check if the number is even
+while(number > 2 ){
+    if (number % 2 == 0){
+        console.log("The number is even.");
+        break;
+    }
+    // if the number is odd
+    else if {
+        console.log(`${number} is too low;`);
+        break;
+    }
+    else (number % 3 == 0 ) {
+        console.log("The number is odd.");
+        break;
+    }
+}
+
+// -------------------------------------------------
+
+// UPPGIFT 5:4
+// let count = 20;
+// while (count >= 2){
+
+//     if ( count % 2 == 0){
+//         console.log (count)
+//     }else{
+//         console.log('')
+//     }
+//     count = count - 1;
+// }
+
+
+// -------------------------------------------------
+
+// UPPGIFT 5:3
+// let count = 0
+
+// while ( count < 5 ) {
+    //     let ord1 = question('enter a number: ')
+    //     let ord2 = question('enter a phrase: ')
+    
+//     if ( ord1 == '' || ord1 == '.') {   
+    //         break;
+    //     }
+//     else if ( ord2 == '' || ord2 == '.') {   
+//         break;
+//     }
+//     else {
+    //         console.log( ord1 + ' ' + ord2 );
+    //     }
+    // }
+    
+// -------------------------------------------------
+
+    // // let result = ord1.concat(" ", ord2); (concat adderar string bakom varann) inte del av lösningen dock)
+
+    
+// -------------------------------------------------
+
+// FIZZ 
+//for-loop från 1 till 20, öker med 1 varje gång
+// for ( let i=1; i<20; i = i + 1) {
+//     const dividesBy3 = i % 3 == 0
+//     const dividesBy5 = i % 5 == 0
+
+//     if( dividesBy3 && (i % 5 != 0)){
+//     console.log('Fizz')
+//     }else if (dividesBy5 && ( i % 3 != 0)){
+//         console.log('Buzz')
+//     }else if (dividesBy3 && dividesBy5){
+//         console.log('FizzBuzz')
+//     }else{
+//         console.log(i)
+//     }
+// }
+// 
+// for (let number = 1; number <=100; number++) {
+
+//     if(number % 15 == 0 )
+//     console.log('Fizzbuzz');
+
+//     else if(number % 3 == 0)
+//     console.log('Fizz')
+
+//     else if(number % 5 ==0)
+//     console.log('Buzz')
+
+//     else
+//     console.log(number)
+// }
+
 // -------------------------------------------------
 
 // UPPGIFT 5C
